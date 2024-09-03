c8538eda24df870cea8592db3053a360866d8bde
neeti123456 <151535868+neeti123456@users.noreply.github.com>
2024-09-03 21:51:17 +0530

    project1

---
+++ index1.js
@@ -0,0 +1,51 @@
+// var -->  update, re-declear 
+
+// let --> update , not re-declear 
+
+// const --> not update, not re-declear 
+
+// variable_type variable_name = value;
+
+// var a = 10;
+// console.log(a);
+
+// a = 100; //update
+// console.log(a);
+
+// var a = 'Aanya'; // re-declear
+// console.log(a);
+
+// let a = 10;
+// console.log(a);
+
+// a = 100; //update
+// console.log(a);
+
+// let a = 'Aanya'; // not re-declear
+// console.log(a);
+
+// const a = 10;
+// console.log(a);
+
+// a = 100; // update
+// console.log(a);
+
+// const a = 'Aanya';
+// console.log(a);
+
+
+// int, number --> 1,5,8,9,2.5,500000,0
+// string --> 'a', "hello", `hello world`, '1+1'
+// boolean --> true, false
+// undefined --> 
+// null --> 
+
+// let a = 'aanya';
+// let a = "aanya";
+// let a = `aanya`;
+// let a = true;
+// let a = null;
+// let a
+// console.log(a);
+
+// console.log(typeof(a));
